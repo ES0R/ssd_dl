008eac70ccd8070e6b0363f93cee53ee8c914539
ES0R <emiloramovic@gmail.com>
2024-11-12 13:58:59 +0000

    init - added the labs + solutions for each topic

---
+++ 01-Large Language Models/LLM Lab/Flask/test.py
@@ -0,0 +1,6 @@
+from helpers import * 
+
+
+
+score = score_candidate("I am a software engineer with 5 years of experience", "We are looking for a software engineer with 5 years of experience")
+print(score)
